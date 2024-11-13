2507980d91be0f6e09cc3fd231a0443cbd704f7f
D1JP <d1jp@TECHIN.INTERNAL>
2024-11-13 12:40:30 +0400

    Initial commit

---
+++ pavyzdziai.js
@@ -0,0 +1,56 @@
+// let arr = ["i", "study", "javascript"];
+
+// arr.splice(1, 1);
+
+// console.log(...arr);
+
+// -------------------------------------
+
+// let n1 = [1, 2, 3, 4, 5, 6, 7];
+// let n2 = n1.map(funkcija);
+
+// function funkcija(value, index, array) {
+//   return value * 2;
+// }
+// console.log(n2);
+
+// -------------------------------------
+
+// const arr = [5, 6, 9, 7, 1, 10, 15, 92]
+
+// const found = arr.find(element => element > 10)
+
+// console.log(found);
+
+// -------------------------------------
+
+// let answer1 = array.reduce(callback, initialValue);
+
+// let answer2 = array.reduce(callback);
+
+// -------------------------------------
+
+// const hero = {
+//     name: 'Batman',
+//     realName: 'Bruce Wayne'
+// };
+
+// const {name, realName} = hero;
+
+// console.log(name);
+// console.log(realName);
+
+// -------------------------------------
+// const name = hero.name;
+// const realName = hero.realName
+
+// // same as 
+
+// const {name, realName} = hero;
+
+// -------------------------------------
+
+
+
+
+
